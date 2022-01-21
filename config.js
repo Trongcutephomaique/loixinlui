6801b3d19044552d320f06b5bbfd13f13ac5bb89
VnHerman <96006850+VnHerman@users.noreply.github.com>
2022-01-21 18:55:26 +0700

    anh xin lui

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Anh xin lũi 🥰",
+    introTitle: 'Bánh mì bé nhỏ',
+    introDesc: `Anh thương em, em đừng dỗi
+    em giận, em ghét, tối anh ngủ sao đây
+    hãy bỏ qua cho anh dẫu lúc này
+    anh không gặp em nói lời xin lỗi`,
+    btnIntro: 'Tha lỗi cho anh nhé',
+    title: 'Tha lỗi cho anh nhé🥰',
+    desc: 'Dẫu biết em có tha lỗi cho anh hay không nhưng anh vẫn muốn nói rằng anh yêu em rất nhiều🥰',
+    btnYes: 'Dễ thương quá tha lỗi lun<33',
+    btnNo: 'Em vẫn giận :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu anh <33333333',
+    mess: 'Anh biết em sẽ tha thứ cho anh mà',
+    messDesc: 'Anh yêu em🥰',
+    btnAccept: 'Gửi cho anh tình yêu của đời anh<3',
+    messLink: '' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
